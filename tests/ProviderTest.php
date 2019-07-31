9ccb59c9dc7eeddc74a7898f323b0a19d200334a
Dan Feder <dafeder@gmail.com>
2019-07-31 16:32:53 -0500

    PSR-2 fixes

--- tests/ProviderTest.php
+++ tests/ProviderTest.php
@@ -1,11 +1,14 @@
 <?php
 
+namespace JsonSchemaProviderTest;
+
 use Swaggest\JsonSchema\Schema;
 
 class ProviderTest extends \PHPUnit\Framework\TestCase
 {
 
-    public function testBasJsonSchema() {
+    public function testBasJsonSchema()
+    {
         $provider = new \JsonSchemaProvider\Provider(new TestSchemaRetriever());
 
         $this->expectExceptionMessage("The requested schema is not valid JSON");
@@ -13,31 +16,44 @@ class ProviderTest extends \PHPUnit\Framework\TestCase
         $provider->retrieve("badjson");
     }
 
-    public function testNotObjectSchema() {
+    public function testNotObjectSchema()
+    {
         $provider = new \JsonSchemaProvider\Provider(new TestSchemaRetriever());
 
-        $this->expectExceptionMessage('The requested schemas is not an object with a reference to a valid meta-schema (ex. { "$schema": "http://json-schema.org/draft-07/schema#" })');
+        $this->expectExceptionMessage(
+            'The requested schemas is not an object with a reference to a valid ' .
+            'meta-schema (ex. { "$schema": "http://json-schema.org/draft-07/schema#" })'
+        );
 
         $provider->retrieve("string");
     }
 
-    public function testNoMetaSchemaSchema() {
+    public function testNoMetaSchemaSchema()
+    {
         $provider = new \JsonSchemaProvider\Provider(new TestSchemaRetriever());
 
-        $this->expectExceptionMessage('The requested schemas is not an object with a reference to a valid meta-schema (ex. { "$schema": "http://json-schema.org/draft-07/schema#" })');
+        $this->expectExceptionMessage(
+            'The requested schemas is not an object with a reference to a valid ' .
+            'meta-schema (ex. { "$schema": "http://json-schema.org/draft-07/schema#" })'
+        );
 
         $provider->retrieve("nometa");
     }
 
-    public function testBadMetaSchemaSchema() {
+    public function testBadMetaSchemaSchema()
+    {
         $provider = new \JsonSchemaProvider\Provider(new TestSchemaRetriever());
 
-        $this->expectExceptionMessage('The requested schemas is not an object with a reference to a valid meta-schema (ex. { "$schema": "http://json-schema.org/draft-07/schema#" })');
+        $this->expectExceptionMessage(
+            'The requested schemas is not an object with a reference to a valid ' .
+            'meta-schema (ex. { "$schema": "http://json-schema.org/draft-07/schema#" })'
+        );
 
         $provider->retrieve("badmeta");
     }
 
-    public function testInvalidAgainstMetaSchema() {
+    public function testInvalidAgainstMetaSchema()
+    {
         $provider = new \JsonSchemaProvider\Provider(new TestSchemaRetriever());
 
         $this->expectExceptionMessage('The requested schema is not valid');
@@ -45,7 +61,8 @@ class ProviderTest extends \PHPUnit\Framework\TestCase
         $provider->retrieve("invalidagainstmeta");
     }
 
-    public function testValidSchema() {
+    public function testValidSchema()
+    {
         $provider = new \JsonSchemaProvider\Provider(new TestSchemaRetriever());
 
         $schema = <<<'JSON'
@@ -59,19 +76,4 @@ JSON;
 
         $this->assertEquals($schema, $provider->retrieve("valid"));
     }
-
-
-}
-
-class TestSchemaRetriever implements \Contracts\Retriever {
-    public function retrieve(string $id): ?string {
-        $file_path = __DIR__ . "/data/{$id}.json";
-        if (file_exists($file_path)) {
-            return file_get_contents($file_path);
-        }
-        else {
-            throw new \Exception("Requested schema {$id} does not exist.");
-        }
-    }
-
 }
